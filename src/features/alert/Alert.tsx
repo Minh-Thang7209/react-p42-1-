fc3c7bd9182a940dc10ec5431b71e501926f3368
Thang <minhthang7209@gmail.com>
2026-07-16 14:09:27 +0300

    add hw

--- src/features/alert/Alert.tsx
+++ src/features/alert/Alert.tsx
@@ -3,12 +3,24 @@ import './ui/Alert.css'
 import AppContext from '../_context/AppContext';
 import type IAlertData from './model/IAlertData';
 
-export default function Alert({data}:{data:IAlertData}) {
-    const {showAlert} = useContext(AppContext);
-    return <div className='alert-bg'>
-        <div className='alert-fg'>
-            {data.message}
-            <button onClick={() => showAlert(null)}>Close</button>
+export default function Alert({ data }: { data: IAlertData }) {
+    const { showAlert } = useContext(AppContext);
+    return (
+        <div className="alert-bg">
+            <div className="alert-fg">
+                <h3 className="alert-title">{data.title}</h3>
+
+                <p className="alert-message">
+                    {data.message}
+                </p>
+
+                <button
+                    className="btn btn-primary"
+                    onClick={() => showAlert(null)}
+                >
+                    Закрити
+                </button>
+            </div>
         </div>
-    </div>
+    );
 }
